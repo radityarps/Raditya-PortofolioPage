53b35f13a66a89b035068d9f35467dd9fe18768e
radityarps <radityarafifps@gmail.com>
2023-08-29 09:32:01 +0700

    create active link navbar

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ window.addEventListener("scroll", () => {
 });
 
 //active navbar onscroll
+let sec = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sec.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
